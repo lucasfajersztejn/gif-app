942124b02eab4cfc77cb455564aadd5c4f9445c4
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-06-05 22:34:24 +0200

    start with testing

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+
+  const [ categories, setCategories ] = useState([ 'One Punch' ]);
+
+  const onAddCategory = (onNewCategory) => {
+    if (categories.includes(onNewCategory)) return
+    setCategories([ onNewCategory, ...categories ]);
+  }
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory
+        onNewCategory={onAddCategory}
+      />
+      
+
+      {
+        categories.map( ( category ) => (
+        <GifGrid key={ category } category={ category }/>
+        ))
+      }
+
+
+    </>
+  )
+}
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+
+export const AddCategory = ({ onNewCategory }) => {
+
+  const [ inputValue, setInputValue ] = useState('')
+
+  const onInputChange = (event) => {
+    setInputValue( event.target.value )
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if ( inputValue.trim().length <= 0 ) return;
+    onNewCategory( inputValue.trim() );
+    setInputValue('')
+  }
+
+  return (
+    <form onSubmit={ onSubmit }>
+      <input 
+        type="text"
+        placeholder="Search gifs"
+        value={ inputValue }
+        onChange={ onInputChange }
+      />
+    </form>
+  )
+}
