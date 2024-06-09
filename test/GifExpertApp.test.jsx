136b59e16b67ccaf64d9533593088183e35a8704
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-06-09 02:06:09 +0200

    adding more tests to <GifExpertApp />

--- test/GifExpertApp.test.jsx
+++ test/GifExpertApp.test.jsx
@@ -3,8 +3,12 @@ import { GifExpertApp } from '../src/GifExpertApp';
 
 
 describe('Pruebas en <GifExpertApp />', () => {
+  // Tratar de escribir en el input del form
+  // Postear el formulario y evaluar el resultado
+  // Tomare el snapshot
+  // Enviar dos veces la misma categoría (No debería de poder enviarse)
 
-  test('', () => {
+  test('Tomar el snapshot', () => {
 
     render( <GifExpertApp /> )
     
